915a5480aedf7a7381c6779706ccff23df9b415e
vlavaka <vronlavs@gmail.com>
2020-08-21 04:07:17 -0600

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,20 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
 
 
-var createTaskHandler = function() {
+  var createTaskHandler = function(event) {
+    
+    event.preventDefault();
+  
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task";
+    listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+  };
+  
