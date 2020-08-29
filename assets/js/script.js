538a8e76d33080e214deff4642e4352efb115e4b
vlavaka <vronlavs@gmail.com>
2020-08-29 00:34:43 -0600

    update info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,21 @@
-var formEl = document.querySelector("#task-form");
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var fromEl = document.querySelector("#task-from");
+var tasksToDoEl = document.querySelector("tasks-to-do");
 
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("sumbit", function() {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
+});
 
-
-  var createTaskHandler = function(event) {
+var createTaskHandler = function(event) {
+    var taskNameInput = document.querySelector("input[name='task-name']");
+    console.log(taskNameInput);
 
     event.preventDefault();
-  
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-  };
-  
+};
